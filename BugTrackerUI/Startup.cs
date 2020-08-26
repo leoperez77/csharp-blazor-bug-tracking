fc0de2113f65d55ac9e0c219e9dfa161031a27ac
Leonardo Perez <leonardo_perez@hotmail.com>
2020-08-25 19:29:40 -0500

    Stpe three

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -25,9 +25,10 @@ namespace BugTrackerUI
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<IBugService, BugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
